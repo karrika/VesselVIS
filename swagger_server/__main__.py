b3a360319edbb2384c02fa72a50b69bbf286be53
Karri Kaksonen <karri@sipo.fi>
2017-03-01 21:12:59 +0000

    Fix instance to work with aws

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -7,10 +7,10 @@ import ssl
 
 if __name__ == '__main__':
     context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-    context.load_cert_chain('Certificate_VIS-IMO8320767.pem',\
-                            'PrivateKey_VIS-IMO8320767.pem')
+    context.load_cert_chain('Certificate_VIS-Falstaff.pem',\
+                            'PrivateKey_VIS-Falstaff.pem')
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Voyage Information Service API facing SeaSWIM through SSC exposing interfaces to SeaSWIM stakeholders'})
-    app.run(port=8002, ssl_context=context)
+    app.run(host='ec2-35-157-50-165.eu-central-1.compute.amazonaws.com', port=443, ssl_context=context)
     #app.run(port=8002)
