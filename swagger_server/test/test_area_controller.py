5544a13ae1d23da969ab63f5ef5e240c440bafa3
karrika <karri@sipo.fi>
2017-04-06 13:40:23 +0300

    Add cleanup

--- swagger_server/test/test_area_controller.py
+++ swagger_server/test/test_area_controller.py
@@ -5,6 +5,8 @@ from __future__ import absolute_import
 from . import BaseTestCase
 from six import BytesIO
 from flask import json
+from pathlib import Path
+import os
 
 
 class TestAreaController(BaseTestCase):
@@ -25,6 +27,19 @@ class TestAreaController(BaseTestCase):
                                     query_string=query_string)
         self.assert200(response, "Response body is : " + response.data.decode('utf-8'))
 
+    def test_upload_area_cleanup(self):
+        """
+        Test case for upload_area cleanup
+
+        
+        """
+        vis2_uvid='urn:mrn:stm:service:instance:furuno:vis2'
+        p = Path('import')
+        files = list(p.glob('**/' + vis2_uvid + '*'))
+        for item in files:
+            print(item)
+            os.remove(str(item))
+        pass
 
 if __name__ == '__main__':
     import unittest
