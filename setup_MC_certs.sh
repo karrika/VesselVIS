4afa75cfc0f28e522a1e06b2f15d7066e21386c2
Karri Kaksonen <karri@sipo.fi>
2017-04-10 09:15:53 +0300

    Add self signed hash links

--- setup_MC_certs.sh
+++ setup_MC_certs.sh
@@ -7,4 +7,11 @@ sudo mkdir -p /usr/share/ca-certificates/MCtest
 sudo cp MCtest/mc-ca-chain.pem /usr/share/ca-certificates/MCtest
 HASHTEST=`openssl x509 -hash -in MCtest/mc-ca-chain.pem -noout`.0
 sudo ln -s /usr/share/ca-certificates/MCproduction/mc-ca-chain.pem /usr/lib/ssl/certs/$HASHPROD
-sudo ln -s /usr/share/ca-certificates/MCproduction/mc-ca-chain.pem /usr/lib/ssl/certs/$HASHTEST
+HASHTEST=70b4092a.0
+sudo ln -s /usr/share/ca-certificates/MCtest/mc-ca-chain.pem /usr/lib/ssl/certs/$HASHTEST
+HASHTEST=83dd6dbc.0
+sudo ln -s /usr/share/ca-certificates/MCtest/mc-ca-chain.pem /usr/lib/ssl/certs/$HASHTEST
+HASHTEST=a6566d95.0
+sudo ln -s /usr/share/ca-certificates/MCtest/mc-ca-chain.pem /usr/lib/ssl/certs/$HASHTEST
+HASHTEST=dfa402ab.0
+sudo ln -s /usr/share/ca-certificates/MCtest/mc-ca-chain.pem /usr/lib/ssl/certs/$HASHTEST
