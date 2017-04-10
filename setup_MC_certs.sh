2e4a4ec4d278d0d8cb0c2579ea516c237b5f33f4
karrika <karri@sipo.fi>
2017-04-10 08:30:38 +0300

    Add hash calculation

--- setup_MC_certs.sh
+++ setup_MC_certs.sh
@@ -2,7 +2,9 @@
 
 sudo mkdir -p /usr/share/ca-certificates/MCproduction
 sudo cp MCproduction/mc-ca-chain.pem /usr/share/ca-certificates/MCproduction
+HASHPROD=`openssl x509 -hash -in MCproduction/mc-ca-chain.pem -noout`.0
 sudo mkdir -p /usr/share/ca-certificates/MCtest
 sudo cp MCtest/mc-ca-chain.pem /usr/share/ca-certificates/MCtest
-sudo ln -s /usr/share/ca-certificates/MCproduction/mc-ca-chain.pem /usr/lib/ssl/certs/24fc69e9.0
-sudo ln -s /usr/share/ca-certificates/MCtest/mc-ca-chain.pem /usr/lib/ssl/certs/dfa402ab.0
+HASHTEST=`openssl x509 -hash -in MCtest/mc-ca-chain.pem -noout`.0
+sudo ln -s /usr/share/ca-certificates/MCproduction/mc-ca-chain.pem /usr/lib/ssl/certs/$HASHPROD
+sudo ln -s /usr/share/ca-certificates/MCproduction/mc-ca-chain.pem /usr/lib/ssl/certs/$HASHTEST
