902bdc1c3d0c28b91ff933303c401577912a4566
Karri Kaksonen <karri@sipo.fi>
2017-06-21 14:31:38 +0300

    Modify vessel conf

--- vesselconf.py
+++ vesselconf.py
@@ -5,7 +5,7 @@ conf['host']='127.0.0.1'
 conf['port']=8100
 conf['stmport']=8000
 conf['id']='urn:mrn:stm:service:instance:furuno:imo7917551'
-conf['name']='M/V Madame Butterfly'
+conf['name']='VIS-MadameButterfly'
 with open('vessel.conf', 'w') as f:
     f.write(json.dumps(conf))
 
