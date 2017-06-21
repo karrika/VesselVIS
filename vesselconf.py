54620fc4e0082efb740ec458282cde4b7d0d238c
Karri Kaksonen <karri@sipo.fi>
2017-06-21 14:28:14 +0300

    Add vessel conf

--- vesselconf.py
+++ vesselconf.py
@@ -3,7 +3,8 @@ import json
 conf={}
 conf['host']='127.0.0.1'
 conf['port']=8100
-conf['id']='urn:mrn:stm:service:instance:furuno:imo8016550'
+conf['stmport']=8000
+conf['id']='urn:mrn:stm:service:instance:furuno:imo7917551'
 conf['name']='M/V Madame Butterfly'
 with open('vessel.conf', 'w') as f:
     f.write(json.dumps(conf))
