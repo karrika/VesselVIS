d60b970bc75834859cae52dd429667f0f861f465
karrika <karri@sipo.fi>
2017-08-28 12:24:03 +0300

    Add simulate_vessel key

--- vesselconf.py
+++ vesselconf.py
@@ -6,6 +6,7 @@ conf['port']=8100
 conf['stmport']=8000
 conf['id']='urn:mrn:stm:service:instance:furuno:imo7917551'
 conf['name']='VIS-MadameButterfly'
+conf['simulate_vessel']=False
 with open('vessel.conf', 'w') as f:
     f.write(json.dumps(conf))
 
