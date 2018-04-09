16be78833d3ccd71ce2b80a7528dc01407aa7956
Karri Kaksonen <karri@sipo.fi>
2018-04-09 07:57:07 +0300

    Create hashes for production

--- setup_MC_certs.sh
+++ setup_MC_certs.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
 
-sudo mkdir -p /usr/share/ca-certificates/MCproduction
-sudo cp MCproduction/mc-ca-chain.pem /usr/share/ca-certificates/MCproduction
-HASHPROD=`openssl x509 -hash -in MCproduction/mc-ca-chain.pem -noout`.0
-sudo mkdir -p /usr/share/ca-certificates/MCstaging
-sudo cp MCstaging/mc-ca-chain.pem /usr/share/ca-certificates/MCstaging
-HASHTEST=`openssl x509 -hash -in MCstaging/mc-ca-chain.pem -noout`.0
-sudo rm -f /usr/lib/ssl/certs/$HASHPROD
-sudo ln -s /usr/share/ca-certificates/MCproduction/mc-ca-chain.pem /usr/lib/ssl/certs/$HASHPROD
+#sudo mkdir -p /usr/share/ca-certificates/MCproduction
+#sudo cp MCproduction/mc-ca-chain.pem /usr/share/ca-certificates/MCproduction
+#HASHPROD=`openssl x509 -hash -in MCproduction/mc-ca-chain.pem -noout`.0
+
+HASHTEST=faed0b4f.0
+sudo rm -f /usr/lib/ssl/certs/$HASHTEST
+sudo ln -s /usr/share/ca-certificates/MCproduction/mc-ca-chain.pem /usr/lib/ssl/certs/$HASHTEST
+
+HASHTEST=401d8821.0
+sudo rm -f /usr/lib/ssl/certs/$HASHTEST
+sudo ln -s /usr/share/ca-certificates/MCproduction/mc-ca-chain.pem /usr/lib/ssl/certs/$HASHTEST
+
+HASHTEST=90a36aaf.0
+sudo rm -f /usr/lib/ssl/certs/$HASHTEST
+sudo ln -s /usr/share/ca-certificates/MCproduction/mc-ca-chain.pem /usr/lib/ssl/certs/$HASHTEST
+
+HASHTEST=f1082ab2.0
+sudo rm -f /usr/lib/ssl/certs/$HASHTEST
+sudo ln -s /usr/share/ca-certificates/MCproduction/mc-ca-chain.pem /usr/lib/ssl/certs/$HASHTEST
+
+HASHTEST=b358665e.0
+sudo rm -f /usr/lib/ssl/certs/$HASHTEST
+sudo ln -s /usr/share/ca-certificates/MCproduction/mc-ca-chain.pem /usr/lib/ssl/certs/$HASHTEST
+
+#sudo mkdir -p /usr/share/ca-certificates/MCstaging
+#sudo cp MCstaging/mc-ca-chain.pem /usr/share/ca-certificates/MCstaging
+
+
 HASHTEST=70b4092a.0
 sudo rm -f /usr/lib/ssl/certs/$HASHTEST
 sudo ln -s /usr/share/ca-certificates/MCstaging/mc-ca-chain.pem /usr/lib/ssl/certs/$HASHTEST
